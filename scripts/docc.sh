7a4001e472de4060e0e16e957d3ba13f378152f0
Daniel Saidi <daniel.saidi@gmail.com>
2024-10-16 14:44:18 +0200

    Simplify DocC script

--- scripts/docc.sh
+++ scripts/docc.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
-
 # This script builds docc with SwiftUI module exports disabled.
 
-# Use the script folder to refer to the other scripts.
-SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-DISABLE="$SCRIPT/docc-swiftui-export-disable.sh"
-BUILD="$SCRIPT/docc-build.sh"
-ENABLE="$SCRIPT/docc-swiftui-export-enable.sh"
+# Variables
+BUILD_FOLDER=".build"
+DERIVED_FOLDER="$BUILD_FOLDER/derived"
+DOCC_FOLDER="$BUILD_FOLDER/docc"
 
-chmod +x "$DISABLE"
-chmod +x "$BUILD"
-chmod +x "$ENABLE"
+# Use the script folder to refer to the rewrite script.
+SCRIPT_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+REWRITE="$SCRIPT_FOLDER/docc-swiftui-export-rewrite.sh"
+chmod +x "$REWRITE"
 
-bash "$DISABLE"
-bash "$BUILD"
-bash "$ENABLE"
+# Disable SwiftUI module export
+bash "$REWRITE" "@_exported import SwiftUI" "import SwiftUI"
+
+# Build DocC documentation
+swift package resolve;
+xcodebuild docbuild -scheme SwiftUIX -derivedDataPath $DERIVED_FOLDER -destination 'generic/platform=iOS';
+
+# Transform the generated documentation for static hosting
+$(xcrun --find docc) process-archive \
+    transform-for-static-hosting $DERIVED_FOLDER/Build/Products/Debug-iphoneos/SwiftUIX.doccarchive \
+    --output-path $DOCC_FOLDER \
+    --hosting-base-path 'SwiftUIX';
+
+# Inject a redirect script into the empty documentation root
+echo "<script>window.location.href += \"/documentation/swiftuix\"</script>" > $DOCC_FOLDER/index.html;
+
+# Ensable SwiftUI module export
+bash "$REWRITE" "import SwiftUI" "@_exported import SwiftUI"
